5401375fbe0f3d349979eb8a76d2e7922a195ab3
Dombovari Peter <dombo.peter@gmail.com>
2017-07-05 19:09:10 +0200

    initial commit

---
+++ src/main/java/com/greenfox/helpmanager/HelpmanagerApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.helpmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpmanagerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/greenfox/helpmanager/HelpmanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfox.helpmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelpmanagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
