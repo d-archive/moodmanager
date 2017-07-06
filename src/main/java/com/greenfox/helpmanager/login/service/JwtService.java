0edd1649ec3e92c022787307606011f80bc5a9f3
Dombovari Peter <dombo.peter@gmail.com>
2017-07-06 13:01:42 +0200

    create jwt service

--- src/main/java/com/greenfox/helpmanager/login/service/JwtService.java
+++ src/main/java/com/greenfox/helpmanager/login/service/JwtService.java
@@ -1,5 +1,35 @@
 package com.greenfox.helpmanager.login.service;
 
-public class JwtService {
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import org.springframework.stereotype.Service;
 
+@Service
+public class JwtService {
+  public String createJwt(String issuer, String subject, long ttlMillis) throws Exception {
+
+    SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    long nowMillis = System.currentTimeMillis();
+    Date now = new Date(nowMillis);
+
+    JwtBuilder builder = Jwts.builder()
+        .setSubject(subject)
+        .setIssuedAt(now)
+        .setIssuer(issuer)
+        .signWith(
+            signatureAlgorithm,
+            "secret".getBytes("UTF-8")
+        );
+
+    if (ttlMillis >= 0) {
+      long expMillis = nowMillis + ttlMillis;
+      Date exp = new Date(expMillis);
+      builder.setExpiration(exp);
+    }
+
+    return builder.compact();
+  }
 }
